e6523b31e68a80e6acfc0e2ae3159978188a6124
PyryV <pyry.vekkeli@helsinki.fi>
2019-02-10 19:19:46 +0200

    frontti lisätty

---
+++ build/precache-manifest.5943120563aaef1681615bfdedb4a9c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81720200833d7a512aa2",
+    "url": "/static/js/main.81720200.chunk.js"
+  },
+  {
+    "revision": "ea5b50ea29bcb9276d2a",
+    "url": "/static/js/1.ea5b50ea.chunk.js"
+  },
+  {
+    "revision": "81720200833d7a512aa2",
+    "url": "/static/css/main.bd24b4bc.chunk.css"
+  },
+  {
+    "revision": "c37970a0f71d696541ebeb87dbf1d1ee",
+    "url": "/index.html"
+  }
+];
